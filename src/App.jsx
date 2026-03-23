25a5e75870a2e664ee136bff74c93d7bbc89a9f7
Gabriel Trejo <gabrielTrejo433@gmail.com>
2026-03-22 23:54:28 -0500

    Add initial project setup with GSAP, Tailwind CSS, and SVG assets
    - Created main application component with GSAP integration.
    - Added global CSS styles with Tailwind CSS configuration.
    - Included custom fonts and theme variables in CSS.
    - Added SVG logos and video assets to the public directory.
    - Set up Vite configuration for React and Tailwind CSS.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+        <div className="text">Welcome to the world of GTA and GSAP</div>
+    </main>
+  )
+}
+
+export default App
